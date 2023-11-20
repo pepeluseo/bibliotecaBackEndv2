6ec127b0b2a2076c45ce63bb29e027f8d945761c
pepeluseo <pepeluseo@gmail.com>
2023-11-20 11:32:23 +0100

    inicio

---
+++ src/main/java/com/accenture/bibliotecabackendfinal/BibliotecaBackEndFinalApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.bibliotecabackendfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaBackEndFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibliotecaBackEndFinalApplication.class, args);
+    }
+
+}
